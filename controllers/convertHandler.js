c94b948e4444a2dc18473c2022464dc9b7751f76
Alex Miserandino <amiserandino20@gmail.com>
2023-08-29 23:54:29 -0700

    added 4 methods

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,61 @@
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
+  this.getNum = function (input) {
+    // get number from input and send to convert function
+    const regexp = /[a-z]/g;
+    const num = input.replace(regexp, "");
+    console.log(num);
+    return Number(num); // might not need to be a number?
   };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
+
+  this.getUnit = function (input) {
+    // get unit from input and send to convert function
+    const regexp = /[0-9.]/g;
+    let unit = input.replace(regexp, "");
+    console.log(unit);
+    return unit;
   };
-  
-  this.getReturnUnit = function(initUnit) {
+
+  this.getReturnUnit = function (initUnit) {
+    const unitObj = {
+      gal: "L",
+      L: "gal",
+      lbs: "kg",
+      kg: "lbs",
+      mi: "km",
+      km: "mi",
+    };
+    for (const key in unitObj) {
+      if (key == initUnit) {
+        console.log(unitObj[key]);
+        return unitObj[key];
+      }
+    }
+  };
+
+  this.spellOutUnit = function (unit) {
     let result;
-    
+
     return result;
   };
 
-  this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
-  };
-  
-  this.convert = function(initNum, initUnit) {
+  this.convert = function (initNum, initUnit) {
+    console.log(initNum)
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
+    if (initUnit == "L") {
+      return initNum / galToL;
+    }
+    if (initUnit == "gal") {
+      return initNum * galToL;
+    }
+  };
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
     let result;
-    
+
     return result;
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
-  };
-  
 }
 
 module.exports = ConvertHandler;
