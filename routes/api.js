e8e1926ace636ee4d0739eab97f56a0441082978
Alex Miserandino <amiserandino20@gmail.com>
2023-08-29 23:55:52 -0700

    added 1 route to mudule

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,19 @@
-'use strict';
+"use strict";
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const expect = require("chai").expect;
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
-  
   let convertHandler = new ConvertHandler();
-
+  app.get("/api/convert", (req, res) => {
+    const input = req.query.input;
+    const initNum = convertHandler.getNum(input);
+    const initUnit = convertHandler.getUnit(input);
+    const returnUnit = convertHandler.getReturnUnit(initUnit)
+    const returnNum = convertHandler.convert(initNum, initUnit)
+    
+    res.json({ initNum, initUnit, returnUnit, returnNum });
+ 
+  });
+ 
 };
