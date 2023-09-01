1e8f6b0d9ac49fda4fa68f4bacfb146fcdda003c
Alex Miserandino <amiserandino20@gmail.com>
2023-09-01 14:07:43 -0700

    finsihed unit tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -53,8 +53,8 @@ suite("Unit Tests", function () {
       "gal",
       "mi",
       "km",
-      "l",
       "lbs",
+      "l",
       "kg",
       "GAL",
       "L",
@@ -63,9 +63,118 @@ suite("Unit Tests", function () {
       "LBS",
       "KG",
     ];
-    input.forEach((element) => {
-      assert.equal(convertHandler.getUnit(1 + element), element.toLowerCase());
+    const outputArr = [
+      "gal",
+      "mi",
+      "km",
+      "lbs",
+      "L",
+      "kg",
+      "gal",
+      "L",
+      "mi",
+      "km",
+      "lbs",
+      "kg",
+    ];
+    input.forEach((element, index) => {
+      assert.equal(convertHandler.getUnit(1 + element), outputArr[index]);
     });
+    done();
+  });
+
+  //8
+  test("Read invalid input unit", (done) => {
+    const input = "35fk";
+    assert.equal(convertHandler.getReturnUnit(input), "invalid unit");
+    done();
+  });
+
+  //9
+  test("Read each valid return unit", (done) => {
+    const unitObj = {
+      gal: "L",
+      L: "gal",
+      lbs: "kg",
+      kg: "lbs",
+      mi: "km",
+      km: "mi",
+    };
+
+    for (const prop in unitObj) {
+      assert.equal(convertHandler.getReturnUnit(prop), unitObj[prop]);
+    }
+    done();
+  });
+
+  //10
+  test("Return spelled out string for valid input unit", (done) => {
+    const input = ["L", "gal", "kg", "lbs", "km", "mi"];
+    const outputArr = [
+      "litres",
+      "gallons",
+      "kilograms",
+      "pounds",
+      "kilometers",
+      "miles",
+    ];
+    input.forEach((element, index) => {
+      assert.equal(convertHandler.spellOutUnit(element), outputArr[index]);
+    });
+    done();
+  });
+
+  //11
+  test("Correctly convert gal to L", (done) => {
+    const initNum = 1;
+    const initUnit = "gal";
+    const result = 3.78541;
+    assert.equal(convertHandler.convert(initNum, initUnit), result);
+    done();
+  });
+
+  //12
+  test("Correctly convert L to gal", (done) => {
+    const initNum = 1;
+    const initUnit = "L";
+    const result = 0.26417;
+    assert.equal(convertHandler.convert(initNum, initUnit), result);
+    done();
+  });
+
+  //13
+  test("Correctly convert mi to km", (done) => {
+    const initNum = 1;
+    const initUnit = "mi";
+    const result = 1.60934;
+    assert.equal(convertHandler.convert(initNum, initUnit), result);
+    done();
+  });
+
+  //14
+  test("Correctly convert km to mi", (done) => {
+    const initNum = 1;
+    const initUnit = "km";
+    const result = 0.62137;
+    assert.equal(convertHandler.convert(initNum, initUnit), result);
+    done();
+  });
+
+  //15
+  test("Correctly convert lbs to kg", (done) => {
+    const initNum = 1;
+    const initUnit = "lbs";
+    const result = 0.45359;
+    assert.equal(convertHandler.convert(initNum, initUnit), result);
+    done();
+  });
+
+  //16
+  test("Correctly convert kg to lbs", (done) => {
+    const initNum = 1;
+    const initUnit = "kg";
+    const result = 2.20462;
+    assert.equal(convertHandler.convert(initNum, initUnit), result);
     done();
   });
 });
