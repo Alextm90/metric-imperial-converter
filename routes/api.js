3cea85a794e12b9f4a3e23e94ea8da4864b19676
Alex Miserandino <amiserandino20@gmail.com>
2023-08-31 23:54:09 -0700

    finished routes

--- routes/api.js
+++ routes/api.js
@@ -9,11 +9,19 @@ module.exports = function (app) {
     const input = req.query.input;
     const initNum = convertHandler.getNum(input);
     const initUnit = convertHandler.getUnit(input);
-    const returnUnit = convertHandler.getReturnUnit(initUnit)
-    const returnNum = convertHandler.convert(initNum, initUnit)
+    const returnUnit = convertHandler.getReturnUnit(initUnit);
+    const returnNum = convertHandler.convert(initNum, initUnit);
+    const string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
     
-    res.json({ initNum, initUnit, returnUnit, returnNum });
- 
+
+    if (initNum == "invalid number" && returnUnit == "badUnit") {
+      res.send("invalid number and unit");
+    } else if (initNum == "invalid number") {
+      res.send("invalid number");
+    } else if (returnUnit == "badUnit") {
+      res.send("invalid unit");
+    } else {
+      res.json({ initNum, initUnit, returnNum, returnUnit, string });
+    }
   });
- 
 };
